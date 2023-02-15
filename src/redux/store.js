501b53cff67dc8fe66c052a3da78bf215b201239
Mateusz-Gonera <mateuszg1992@wp.pl>
2023-02-15 12:20:17 +0100

    add folders

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
