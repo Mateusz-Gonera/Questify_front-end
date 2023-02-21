861e9cfcd7cca23ee6c219196e37f8c2a26fb173
KacperPasnicki <kacper.pas@hotmail.com>
2023-02-21 23:59:44 +0100

    dodany edytowalny input

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,18 @@ import { Provider } from 'react-redux';
 import App from "./components/App";
 import "./index.css";
 import { store } from "./redux/store.js";
+<<<<<<< Updated upstream
 
+=======
+import  Modal from "./components/modal/modal.jsx"
+>>>>>>> Stashed changes
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<BrowserRouter>
 				<App />
+				<Modal/>
 			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>,
