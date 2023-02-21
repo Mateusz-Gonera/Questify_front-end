01f577d1ad52bf1d4924cf9050e88c2341687fd7
Mateusz-Gonera <mateuszg1992@wp.pl>
2023-02-21 13:44:41 +0100

    commit

--- src/components/landing/landingWelcome/LandingWelcome.jsx
+++ src/components/landing/landingWelcome/LandingWelcome.jsx
@@ -1,20 +1,22 @@
-import { Wrapper } from "./LandingWelcome.styled"
+import { Wrapper } from "./LandingWelcome.styled";
 
 const LandingWelcome = () => {
-  return (
-    <Wrapper>
-      <h1>Questify</h1>
-      <p>
-        Questify will turn your life into a thrilling game full of amazing
-        quests and exciting challenges.
-      </p>
+	return (
+		<Wrapper>
+			<h1>Questify</h1>
+			<p>
+				Questify will turn your life into a thrilling game full of amazing
+				quests and exciting challenges.
+			</p>
 
-      <br/>
-      
-      <p>Choose your name to <span className="textButton">sign up</span> or <span className="textButton">log in</span> <button> go! </button></p>
-    
-    </Wrapper>
-  );
+			<br />
+
+			<p>
+				Choose your name to <span className="textButton">sign up</span> or{" "}
+				<span className="textButton">log in</span> <button> go! </button>
+			</p>
+		</Wrapper>
+	);
 };
 
 export default LandingWelcome;
