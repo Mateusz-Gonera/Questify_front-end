dca7c4907a853504377f81eb5064c49dce26e5b7
Andrzej Markiewicz <endercom502@gmail.com>
2023-02-22 22:25:09 +0100

    start

--- src/components/CardsList/CardsList.jsx
+++ src/components/CardsList/CardsList.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import style from "./CardsList.module.css"
-import Clock from "./Clock"
-import TodayCards from './TodayCards';
-import TomorrowCards from './TomorrowCards';
+import React from "react";
+import style from "./CardsList.module.css";
+import Clock from "./Clock";
+import TodayCards from "./TodayCards";
+import TomorrowCards from "./TomorrowCards";
 
-function CardsList({cards}) {
+function CardsList({ cards }) {
+  const addCard = () => {
+    const newCard = {
+      isChallenge: false,
+      difficulty: "Normal",
+      group: "Work",
+      title: "New Challenge",
+    };
+    setCards([...cards, newCard]);
+  };
 
-   const addCard = () => {
-     const newCard = {
-       isChallenge: false,
-       difficulty: 'Normal',
-       group: 'Work',
-       title: 'New Challenge'
-     };
-     setCards([...cards, newCard]);
-   };
-
-   return(
-      <div className={style.sectionCards}>
-         <button onClick={addCard}>Add Card</button>
-         <Clock/>
-         <TodayCards cards={cards}/>
-         <TomorrowCards cards={cards}/>
-         
-      </div>
-   )
+  return (
+    <div className={style.sectionCards}>
+      <button className={style.btn_add_card} onClick={addCard}>
+        +
+      </button>
+      <Clock />
+      <TodayCards cards={cards} />
+      <TomorrowCards cards={cards} />
+    </div>
+  );
 }
 
-export default CardsList
+export default CardsList;
