4ffd04f65e63218b3cefbe8076fb2b69adf9b468
anton <toxaf13@gmail.com>
2023-02-22 20:31:55 +0100

    2.1

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react';
-
-
-import { useState } from 'react';
 import style from './Cards.module.css';
 
 function Card({
@@ -13,45 +10,10 @@ function Card({
  dueTime,
 }) {
 
-   const [title, setTitle] = useState(initialTitle);
-  const [selectedDifficulty, setSelectedDifficulty] = useState(difficulty);
-  const [showDifficultyDropdown, setShowDifficultyDropdown] = useState(false);
-
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
-  };
-
-  const handleDifficultyClick = () => {
-    setShowDifficultyDropdown(!showDifficultyDropdown);
-  };
-
-  const handleDifficultyChange = (event) => {
-    setSelectedDifficulty(event.target.value);
-    setShowDifficultyDropdown(false);
-  };
-
-   //
-const today = new Date();
-  const tomorrow = new Date(today);
-  tomorrow.setDate(tomorrow.getDate() + 1);
-
-  const formatDate = (date) => {
-    if (date.toDateString() === today.toDateString()) {
-      return "Today";
-    } else if (date.toDateString() === tomorrow.toDateString()) {
-      return "Tomorrow";
-    } else {
-      return date.toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: 'short',
-      day: 'numeric',
-    });
-    }
-  };
   const [title, setTitle] = useState(initialTitle);
   const [selectedDifficulty, setSelectedDifficulty] = useState(difficulty);
   const [showDifficultyDropdown, setShowDifficultyDropdown] = useState(false);
-  const [selectedGroup, setSelectedGroup] = useState(group);
+  const [selectedGroup, setSelectedGroup] = useState(category);
   const [showGroupDropdown, setShowGroupDropdown] = useState(false);
   const [endDate, setEndDate] = useState(null);
 
@@ -59,7 +21,6 @@ const today = new Date();
     setTitle(event.target.value);
   };
 
-
   const handleDifficultyClick = () => {
     setShowDifficultyDropdown(!showDifficultyDropdown);
   };
@@ -68,7 +29,6 @@ const today = new Date();
     setSelectedDifficulty(event.target.value);
     setShowDifficultyDropdown(false);
   };
-
   const handleGroupChange = (event) => {
     setSelectedGroup(event.target.value);
     setShowGroupDropdown(false);
@@ -90,94 +50,95 @@ const today = new Date();
     function handleResetEndDate() {
     setEndDate(null);
     }
-    
-  
+
+   //
+const today = new Date();
+  const tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  const formatDate = (date) => {
+    if (date.toDateString() === today.toDateString()) {
+      return "Today";
+    } else if (date.toDateString() === tomorrow.toDateString()) {
+      return "Tomorrow";
+    } else {
+      return date.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    });
+    }
+  };
 
    return (
-   <li>
-      <div className={isChallenge ? style.challengeContainer : style.cardContainer}>
-         <div className={style.difficultyContainer}>
-            <div className={style.difficultyLevel}>
-               {/* rome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
-                  <h3 className={style.levelName} onClick={handleDifficultyClick}>
-                     {selectedDifficulty}
-                  </h3>
-                  {showDifficultyDropdown && (
-                     <select
+<li>
+   <div className={isChallenge ? style.challengeContainer : style.cardContainer}>
+      <div className={style.difficultyContainer}>
+         <div className={style.difficultyLevel}>
+            {/* rome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+               <h3 className={style.levelName} onClick={handleDifficultyClick}>
+                  {selectedDifficulty}
+               </h3>
+               {showDifficultyDropdown && (
+                  <select
                      className={style.difficultyDropdown}
                      value={selectedDifficulty}
-                     onChange={handleDifficultyChange}
-                        size="3"
-                     >
-                     <option value="Easy" >Easy</option>
-                     <option value="Normal">Normal</option>
-                     <option value="Hard">Hard</option>
-                     </select>
-                )}
-
-            </div>
-
-
-            {isChallenge ? ( <button className={style.trophyIcon}> </button> )
-            : ( <button className={style.starIcon}> </button> )}
+                     onChange={handleDifficultyChange} size="3">
+                        <option value="Easy" >Easy</option>
+                        <option value="Normal">Normal</option>
+                        <option value="Hard">Hard</option>
+                  </select>
+               )}
          </div>
-            <div className={style.titleContainer} >
-               {isChallenge && <h3 className={style.isChallenge} >Challenge</h3>}
 
-               <input type="text" 
-                  className={isChallenge ? style.chalengeName : style.taskName} 
-                  value={title}
-                     onChange={handleTitleChange} />
-               <h5 className={style.date}>{formatDate(new Date(dueDate))} , {dueTime} </h5>
-            </div>
-         <div className={style[category]}>{category}</div>
+
+         {isChallenge ? ( <button className={style.trophyIcon}> </button> )
+            : ( <button className={style.starIcon}> </button> )}
       </div>
-     
-      {isChallenge && <button className={style.isChallenge}>Challenge</button>}
-      <input
-        type="text"
-        className={isChallenge ? style.chalengeName : style.taskName}
-        value={title}
-        onChange={handleTitleChange}
-        
-      />
+      <div className={style.titleContainer} >
+         {isChallenge && <button className={style.isChallenge}>Challenge</button>}
+         <input type="text" 
+            className={isChallenge ? style.chalengeName : style.taskName} 
+            value={title} onChange={handleTitleChange} />
 
-<div>
 
-{endDate ? (
-<div>
-{/* rome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
-<p onClick={handleResetEndDate}> {endDate}</p>
+         <h5 className={style.date}>{formatDate(new Date(dueDate))} , {dueTime} </h5>
 
-</div>
-) : (
-<form onSubmit={handleEndDateSubmit}>
-<label>
+         <div>
+               {endDate ? (
+                  <div>
+                     {/* rome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+                     <p className={style.date} onClick={handleResetEndDate}> {endDate}</p>
+                  </div>
+                  ) : (
+                  <form onSubmit={handleEndDateSubmit}>
+                     <label>
+                        <input type="datetime-local" onChange={handleEndDateChange} />
+                     </label>
+                     {/* <button type="submit">OK</button> */}
+                  </form>
+               )}
+            </div>
 
-<input type="datetime-local" onChange={handleEndDateChange} />
-</label>
-{/* <button type="submit">OK</button> */}
-</form>
-)}
-</div>
+
+      </div>
       {/* rome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
-<div className={style[selectedGroup]} onClick={handleGroupClick}>{selectedGroup}
-{showGroupDropdown && (
-        <select
-        className={style.difficultyDropdown}
-        value={selectedGroup}
-        onChange={handleGroupChange}
-         size="6">
-          <option value="Stuff">Stuff</option>
-          <option value="Family">Family</option>
-          <option value="Health">Health</option>
-          <option value="Learning">Learning</option>
-          <option value="Leisure">Leisure</option>
-          <option value="Work">Work</option>
-        </select>
-      )}
- </div>
-    </div>
+      <div className={style[selectedGroup]} onClick={handleGroupClick}>{selectedGroup}
+         {showGroupDropdown && (
+         <select
+            className={style.difficultyDropdown}
+            value={selectedGroup}
+            onChange={handleGroupChange} size="6">
+               <option value="Stuff">Stuff</option>
+               <option value="Family">Family</option>
+               <option value="Health">Health</option>
+               <option value="Learning">Learning</option>
+               <option value="Leisure">Leisure</option>
+               <option value="Work">Work</option>
+         </select>
+         )}
+      </div>
+   </div>
+</li>
   );
 }
 
--- src/components/CardsList/CardsList.jsx
+++ src/components/CardsList/CardsList.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import Card from "../Cards/Cards"
+import React from 'react';
 import style from "./CardsList.module.css"
 import Clock from "./Clock"
+import TodayCards from './TodayCards';
+import TomorrowCards from './TomorrowCards';
 
 function CardsList({cards}) {
 
- 
    const addCard = () => {
      const newCard = {
        isChallenge: false,
@@ -15,94 +15,13 @@ function CardsList({cards}) {
      };
      setCards([...cards, newCard]);
    };
-//
-  const currentDate = new Date();
-  const tomorrowDate = new Date(currentDate.getTime() + 24 * 60 * 60 * 1000);
-  
-  const filteredCards = cards.filter((card) => {
-    const cardDate = new Date(card.date);
-    return (
-      cardDate.getFullYear() === currentDate.getFullYear() &&
-      cardDate.getMonth() === currentDate.getMonth() &&
-      cardDate.getDate() === currentDate.getDate()
-    );
-  });
-  const todayCards = filteredCards.sort((a, b) => {
-   const timeA = timeStringToMs(a.time);
-		const timeB = timeStringToMs(b.time);
-      console.log(timeA)
-      console.log(timeB)
-		if (timeA < timeB) {
-			return -1;
-		} else if (timeA > timeB) {
-			return 1;
-		} else {
-			return 0;
-		}
-  });
-  const filteredTomorrowCards = cards.filter((card) => {
-    const cardDate = new Date(card.date);
-    return (
-      cardDate.getFullYear() === tomorrowDate.getFullYear() &&
-      cardDate.getMonth() === tomorrowDate.getMonth() &&
-      cardDate.getDate() === tomorrowDate.getDate()
-    );
-  });
- 
-const tomorrowCards = filteredTomorrowCards.sort((a, b) => {
-		const timeA = timeStringToMs(a.time);
-		const timeB = timeStringToMs(b.time);
-      console.log(timeA)
-      console.log(timeB)
-		if (timeA < timeB) {
-			return -1;
-		} else if (timeA > timeB) {
-			return 1;
-		} else {
-			return 0;
-		}
-	});
-function timeStringToMs(cards) {
-  const [hours, minutes] = cards.split("-");
-  const date = new Date();
-  date.setHours(hours);
-  date.setMinutes(minutes);
-  return date.getTime();
-}
 
    return(
       <div className={style.sectionCards}>
          <button onClick={addCard}>Add Card</button>
          <Clock/>
-         <div className={style.todaylist}>
-            <h4>Today</h4>
-            <ul className={style.cardList}>
-            {todayCards.map((card) => (
-               <Card key={card.id} 
-               title={card.title} 
-               dueDate={card.date} 
-               dueTime={card.time}
-               isChallenge={card.isChallenge}
-               difficulty={card.difficulty}
-               category={card.category}
-               /> ))}
-         </ul>
-         </div>
-         <div className={style.tomorrowList}>
-            <h4>Tomorrow </h4>
-               <ul className={style.cardList}>
-               {tomorrowCards.map((card) => (
-                  <Card key={card.id} 
-                  title={card.title} 
-                  dueDate={card.date} 
-                  dueTime={card.time}
-                  isChallenge={card.isChallenge}
-                  difficulty={card.difficulty}
-                  category={card.category}
-                  />
-                  ))}
-               </ul>
-         </div>
+         <TodayCards cards={cards}/>
+         <TomorrowCards cards={cards}/>
          
       </div>
    )
