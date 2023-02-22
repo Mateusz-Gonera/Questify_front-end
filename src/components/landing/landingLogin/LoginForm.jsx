092b3597307fafb21421399d44f84879581b08f3
Mateusz-Gonera <mateuszg1992@wp.pl>
2023-02-22 20:55:39 +0100

    fix critical error

--- src/components/landing/landingLogin/LoginForm.jsx
+++ src/components/landing/landingLogin/LoginForm.jsx
@@ -1,66 +1,66 @@
 import styles from "./LoginForm.module.css";
 import { Form, Button, Container } from "react-bootstrap";
-
 import { useLoginMutation } from "../../../redux/api/questifyApi";
-
 import { useNavigate } from "react-router-dom";
 
 export const LoginForm = () => {
-  
-  const [login] = useLoginMutation();
-  const navigate = useNavigate();
+	const [login] = useLoginMutation();
+	const navigate = useNavigate();
 
-  const handleSubmit = async evt => {
-    evt.preventDefault();
-    const form = evt.currentTarget;
-    const email = form.elements.email.value;
-    const password = form.elements.password.value;
-    
-    const credentials = { email, password };
-    await login(credentials)
-    unwrap()
-      .then(({ accessToken,
-        userData: { email, id, cards }, }) => localStorage.setItem('token', accessToken))
-      .catch(() => {
-        toast.warn('Please check your email address or password', {
-          icon: "🦄", theme: "dark"
-        });
-      });
+	const handleSubmit = async (evt) => {
+		evt.preventDefault();
+		const form = evt.currentTarget;
+		const email = form.elements.email.value;
+		const password = form.elements.password.value;
 
-    const token = localStorage.getItem('token');
-    dispatch(login(token))
-    navigate("/dashboard")
-    form.reset();
-   
-  };
+		const credentials = { email, password };
+		await login(credentials)
+			.unwrap()
+			.then(({ accessToken, userData: { email, id, cards } }) =>
+				localStorage.setItem("token", accessToken),
+			)
+			.catch(() => {
+				toast.warn("Please check your email address or password", {
+					icon: "🦄",
+					e: "dark",
+				});
+			});
 
-  return (
-    <Container className={styles.container}>
-      <Form className="border rounded p-4" onSubmit={handleSubmit}>
-        <p className={styles.paragraph}>Please log in</p>
-        <Form.Group className="mb-3" controlId="inputEmail">
-          <Form.Label>E-mail</Form.Label>
-          <Form.Control
-            className={styles.input}
-            type="email"
-            name="email"
-            title="For example: example@email.com"
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="inputPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            className={styles.input}
-            type="password"
-            name="password"
-            title="Minimum 4 characters"
-            minLength="4"
-            required
-          />
-        </Form.Group>
-        <Button className={styles.buttonGo} type="submit">go!</Button>
-      </Form>
-    </Container>
-  );
+		const token = localStorage.getItem("token");
+		await login(token);
+		navigate("/dashboard");
+		form.reset();
+	};
+
+	return (
+		<Container className={styles.container}>
+			<Form className="border rounded p-4" onSubmit={handleSubmit}>
+				<p className={styles.paragraph}>Please log in</p>
+				<Form.Group className="mb-3" controlId="inputEmail">
+					<Form.Label>E-mail</Form.Label>
+					<Form.Control
+						className={styles.input}
+						type="email"
+						name="email"
+						title="For example: example@email.com"
+						required
+					/>
+				</Form.Group>
+				<Form.Group className="mb-3" controlId="inputPassword">
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						className={styles.input}
+						type="password"
+						name="password"
+						title="Minimum 4 characters"
+						minLength="4"
+						required
+					/>
+				</Form.Group>
+				<Button className={styles.buttonGo} type="submit">
+					go!
+				</Button>
+			</Form>
+		</Container>
+	);
 };
