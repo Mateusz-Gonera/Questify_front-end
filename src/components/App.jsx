8784cda46665d90f7e9c0b6d936781575d6678ab
Mateusz-Gonera <mateuszg1992@wp.pl>
2023-02-23 15:31:08 +0100

    fix problem

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,35 +11,34 @@ import Register from "../pages/landingPage/Register";
 import Login from "../pages/landingPage/Login";
 
 const Dashboard = React.lazy(() => import("../pages/dashboard/Dashboard"));
-const LandingPage = React.lazy(() =>
-  import("../pages/landingPage/LandingPage")
+const LandingPage = React.lazy(
+	() => import("../pages/landingPage/LandingPage"),
 );
 
 const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<LandingWelcome />} />
-          <Route path="register" element={<Register />} />
-          <Route
-            path="login"
-            element={
-              <RestrictedRoute redirectTo="/dashboard" component={<Login />} />
-            }
-          />
-          <Route
-            path="dashboard"
-            element={
-              <PrivateRoute redirectTo="/login" component={<Dashboard />} />
-            }
-          />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<LandingWelcome />} />
+					<Route path="register" element={<Register />} />
+					<Route
+						path="login"
+						element={
+							<RestrictedRoute redirectTo="/dashboard" component={<Login />} />
+						}
+					/>
+					<Route
+						path="dashboard"
+						element={
+							<PrivateRoute redirectTo="/login" component={<Dashboard />} />
+						}
+					/>
+					<Route path="*" element={<NotFound />} />
+				</Route>
+			</Routes>
+		</>
+	);
 };
 
 export default App;
-
