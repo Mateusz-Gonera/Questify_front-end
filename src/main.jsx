7d95103bb48f6f4ebdc101b3f7a666a6cfe55a05
Małgorzata Gąbka <malgorzata.dycha@gmail.com>
2023-02-25 14:31:44 +0100

    logged in

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./components/App";
 import "./index.css";
-import { store } from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
 			<BrowserRouter>
 				<App />
-				
-			</BrowserRouter>
+				</BrowserRouter>
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>,
 );
