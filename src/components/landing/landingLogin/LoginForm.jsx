595799f7871f7801186077373b5009c92f29ef9e
Andrzej Markiewicz <endercom502@gmail.com>
2023-02-25 22:35:04 +0100

    Update LoginForm.jsx

--- src/components/landing/landingLogin/LoginForm.jsx
+++ src/components/landing/landingLogin/LoginForm.jsx
@@ -1,19 +1,3 @@
-<<<<<<< Updated upstream
-import styles from "./LoginForm.module.css";
-import { Form, Button, Container } from "react-bootstrap";
-import { useLoginMutation } from "../../../redux/api/questifyApi";
-import { useNavigate } from "react-router-dom";
-
-export const LoginForm = () => {
-	const [login] = useLoginMutation();
-	const navigate = useNavigate();
-
-	const handleSubmit = async (evt) => {
-		evt.preventDefault();
-		const form = evt.currentTarget;
-		const email = form.elements.email.value;
-		const password = form.elements.password.value;
-=======
 import styles from './LoginForm.module.css';
 import { Form, Button, Container } from 'react-bootstrap';
 import { useLoginMutation } from '../../../redux/api/questifyApi';
@@ -32,7 +16,6 @@ export const LoginForm = () => {
 
   const handleSubmit = async evt => {
     evt.preventDefault();
->>>>>>> Stashed changes
 
 		const credentials = { email, password };
 		await login(credentials)
@@ -53,39 +36,6 @@ export const LoginForm = () => {
 		form.reset();
 	};
 
-<<<<<<< Updated upstream
-	return (
-		<Container className={styles.container}>
-			<Form className="border rounded p-4" onSubmit={handleSubmit}>
-				<p className={styles.paragraph}>Please log in</p>
-				<Form.Group className="mb-3" controlId="inputEmail">
-					<Form.Label>E-mail</Form.Label>
-					<Form.Control
-						className={styles.input}
-						type="email"
-						name="email"
-						title="For example: example@email.com"
-						required
-					/>
-				</Form.Group>
-				<Form.Group className="mb-3" controlId="inputPassword">
-					<Form.Label>Password</Form.Label>
-					<Form.Control
-						className={styles.input}
-						type="password"
-						name="password"
-						title="Minimum 4 characters"
-						minLength="4"
-						required
-					/>
-				</Form.Group>
-				<Button className={styles.buttonGo} type="submit">
-					go!
-				</Button>
-			</Form>
-		</Container>
-	);
-=======
     await login(credentials)
       .unwrap()
       .then(({ accessToken, userData: { email, id, cards } }) => {
@@ -136,5 +86,4 @@ export const LoginForm = () => {
       </Form>
     </Container>
   );
->>>>>>> Stashed changes
 };
