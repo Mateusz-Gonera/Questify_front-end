b3184d9aeb7a8bc8fbc58a13b4c0d32965b57c62
Andrzej Markiewicz <endercom502@gmail.com>
2023-02-25 22:05:12 +0100

    add

--- src/components/landing/landingLogin/LoginForm.jsx
+++ src/components/landing/landingLogin/LoginForm.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import styles from "./LoginForm.module.css";
 import { Form, Button, Container } from "react-bootstrap";
 import { useLoginMutation } from "../../../redux/api/questifyApi";
@@ -12,6 +13,26 @@ export const LoginForm = () => {
 		const form = evt.currentTarget;
 		const email = form.elements.email.value;
 		const password = form.elements.password.value;
+=======
+import styles from './LoginForm.module.css';
+import { Form, Button, Container } from 'react-bootstrap';
+import { useLoginMutation } from '../../../redux/api/questifyApi';
+import { toast } from 'react-toastify';
+// import { useDispatch } from "react-redux";
+
+// import {
+//   setUser,
+//   setToken,
+
+// } from "../../../redux/slices/authSlice";
+
+export const LoginForm = () => {
+  //   const dispatch = useDispatch();
+  const [login] = useLoginMutation();
+
+  const handleSubmit = async evt => {
+    evt.preventDefault();
+>>>>>>> Stashed changes
 
 		const credentials = { email, password };
 		await login(credentials)
@@ -32,6 +53,7 @@ export const LoginForm = () => {
 		form.reset();
 	};
 
+<<<<<<< Updated upstream
 	return (
 		<Container className={styles.container}>
 			<Form className="border rounded p-4" onSubmit={handleSubmit}>
@@ -63,4 +85,56 @@ export const LoginForm = () => {
 			</Form>
 		</Container>
 	);
+=======
+    await login(credentials)
+      .unwrap()
+      .then(({ accessToken, userData: { email, id, cards } }) => {
+        localStorage.setItem('token', accessToken);
+        // if (accessToken) {
+        //   dispatch(setUser({ email, id, cards }));
+        // }
+      })
+      .catch(() => {
+        toast.warn('Please check your email address or password', {
+          theme: 'dark',
+        });
+      });
+    // const token = localStorage.getItem("token");
+
+    // dispatch(setToken(token));
+    form.reset();
+  };
+
+  return (
+    <Container className={styles.container}>
+      <Form className="border rounded p-4" onSubmit={handleSubmit}>
+        <p className={styles.paragraph}>Please log in</p>
+        <Form.Group className="mb-3" controlId="inputEmail">
+          <Form.Label>E-mail</Form.Label>
+          <Form.Control
+            className={styles.input}
+            type="email"
+            name="email"
+            title="For example: example@email.com"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="inputPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            className={styles.input}
+            type="password"
+            name="password"
+            title="Minimum 4 characters"
+            minLength="4"
+            required
+          />
+        </Form.Group>
+        <Button className={styles.buttonGo} type="submit">
+          go!
+        </Button>
+      </Form>
+    </Container>
+  );
+>>>>>>> Stashed changes
 };
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import CardList from "../../components/CardsList/CardsList";
 
 const Dashboard = () => {
@@ -15,6 +16,30 @@ const Dashboard = () => {
 			<CardList  cards={cards} />
 		</div>
 	);
+=======
+import { useGetAllCardsQuery } from '../../redux/api/questifyApi';
+import CardList from '../../components/CardsList/CardsList';
+import { AppBar } from '../../components/appBar/AppBar';
+const Dashboard = () => {
+  //   const cards = [
+  //   { id: 1, title: "Buy the gift for Mary", date: "2023-02-23",time:"08-10", isChallenge: true, difficulty:"Normal", category:"Stuff" },
+  //   { id: 2, title: "Webinar", date: "2023-02-23",time:"20-30", isChallenge: false, difficulty:"Hard", category:"Health" },
+  //   { id: 3, title: "Read an article", date: "2023-02-22",time:"15-25", isChallenge: false, difficulty:"Easy", category:"Family" },
+  //   { id: 4, title: "Watch a movie", date: "2023-02-23",time:"12-15", isChallenge: false, difficulty:"Normal", category:"Learning" },
+  //   { id: 5, title: "Watch a movie",date: "2023-02-22",time:"11-30", isChallenge: false, difficulty:"Hard", category:"Leisure" },
+  //   { id: 6, title: "Morning run", date: "2023-02-22",time:"09-45", isChallenge: false, difficulty:"Easy", category:"Work" },
+  // ];
+  const { data, error, isLoading, isSuccess } = useGetAllCardsQuery();
+  if (!data) return;
+  const cards = data.cards;
+
+  return (
+    <div>
+      <AppBar />
+      <CardList cards={cards} />
+    </div>
+  );
+>>>>>>> Stashed changes
 };
 
 export default Dashboard;
