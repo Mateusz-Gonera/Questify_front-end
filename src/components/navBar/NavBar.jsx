d18b2285c4e59ff53985c2a6ba0a7850a9d9fd0d
Mateusz-Gonera <mateuszg1992@wp.pl>
2023-02-26 22:37:08 +0100

    add name and avatarURL to user state

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,18 +1,14 @@
+import style from "../navBar/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 import { useAuth } from "../../utils/hooks/useAuth";
-
-import Avatar from "../../assets/header/avatar.svg";
+import { useLogoutMutation } from "../../redux/api/questifyApi";
 import ChallengeIcon from "../../assets/header/challengeIcon.svg";
 import Logout from "../../assets/header/logout.svg";
 
-import { useLogoutMutation } from "../../redux/api/questifyApi";
-
-import style from "../navBar/NavBar.module.css";
-
 export const NavBar = () => {
-	
 	const [logout] = useLogoutMutation();
 	const { user } = useAuth();
+
 	return (
 		<>
 			<NavLink className={style.link} to="/">
@@ -21,14 +17,26 @@ export const NavBar = () => {
 			<nav className={style.navWrapper}>
 				<div className={style.userMenu}>
 					<div className={style.avatar}>
-						<img src={Avatar} alt="user avatar" />
+						<img
+							className={style.avatarImg}
+							src={user.avatarURL}
+							alt="user avatar"
+						/>
 					</div>
-					<p className={style.userName}>{user.email} Quest Log </p>
+					<p className={style.userName}>{user.name}'s Quest Log </p>
 				</div>
 				<img className={style.challengeIcon} src={ChallengeIcon} alt="logo" />
-			    <NavLink to="/">
-					{ <button className={style.btn} type="button" onClick={() => logout()} ><img src={Logout} /></button> }
-                </NavLink>
+				<NavLink to="/">
+					{
+						<button
+							className={style.btn}
+							type="button"
+							onClick={() => logout()}
+						>
+							<img src={Logout} alt="logout button" />
+						</button>
+					}
+				</NavLink>
 			</nav>
 		</>
 	);
