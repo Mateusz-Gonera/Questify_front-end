ea1ef01ff249f43beb95e073c12b5a7a42c1c672
Mateusz-Gonera <mateuszg1992@wp.pl>
2023-03-04 11:34:36 +0100

    fix trophy challange

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -3,58 +3,59 @@ import { NavLink } from "react-router-dom";
 import { useAuth } from "../../utils/hooks/useAuth";
 import { useLogoutMutation } from "../../redux/api/questifyApi";
 import ChallengeIcon from "../../assets/header/challengeIcon.svg";
-import Trophy from "../../assets/header/trophy.svg";
+import Trophy from "../../assets/header/challenge_icon_not_active.png";
+import img from "../../assets/header/trophy.svg";
 import Logout from "../../assets/header/logout.svg";
 import { useGetAllCardsQuery } from "../../redux/api/questifyApi";
 
 export const NavBar = () => {
-  const [logout] = useLogoutMutation();
-  const { user } = useAuth();
-  const { data } = useGetAllCardsQuery();
+	const [logout] = useLogoutMutation();
+	const { user } = useAuth();
+	const { data } = useGetAllCardsQuery();
 
-  const onlyChallenge = data.cards.some((card) => card.type === "Challenge" && card.status === "Incomplete");
+	const onlyChallenge = data.cards.some(
+		(card) => card.type === "Challenge" && card.status === "Incomplete",
+	);
 
-  return (
-    <>
-      <NavLink className={style.logo} to="/">
-        Questify
-      </NavLink>
-      <nav className={style.navWrapper}>
-        <div className={style.userMenu}>
-          <div className={style.avatar}>
-            <img
-              className={style.avatarImg}
-              src={user.avatarURL}
-              alt="user avatar"
-            />
-          </div>
-          <p className={style.userName}>{user.name}'s Quest Log </p>
-        </div>
+	return (
+		<>
+			<NavLink className={style.logo} to="/">
+				Questify
+			</NavLink>
+			<nav className={style.navWrapper}>
+				<div className={style.userMenu}>
+					<div className={style.avatar}>
+						<img
+							className={style.avatarImg}
+							src={user.avatarURL}
+							alt="user avatar"
+						/>
+					</div>
+					<p className={style.userName}>{user.name}'s Quest Log </p>
+				</div>
 
-        {onlyChallenge === true ? (
-          <img className={style.challengeIcon} src={ChallengeIcon} alt="logo" />
-        ) : (
-          <div className={style.trophyBg}>
-            <img className={style.trophyIcon} src={Trophy} alt="logo" />
-          </div>
-        )}
+				{onlyChallenge === true ? (
+					<img className={style.challengeIcon} src={ChallengeIcon} alt="logo" />
+				) : (
+					<img className={style.trophyIcon} src={Trophy} alt="logo" />
+				)}
 
-        <NavLink to="/">
-          {
-            <button
-              className={style.btn}
-              type="button"
-              onClick={() => logout()}
-            >
-              <img
-                src={Logout}
-                className={style.logoutIcon}
-                alt="logout button"
-              />
-            </button>
-          }
-        </NavLink>
-      </nav>
-    </>
-  );
+				<NavLink to="/">
+					{
+						<button
+							className={style.btn}
+							type="button"
+							onClick={() => logout()}
+						>
+							<img
+								src={Logout}
+								className={style.logoutIcon}
+								alt="logout button"
+							/>
+						</button>
+					}
+				</NavLink>
+			</nav>
+		</>
+	);
 };
